b5e5a8be59e0a7bc276d5478ae2741742e25e380
Gabriel M <bielsantos99@gmail.com>
2022-01-21 17:13:22 -0300

    Changed controller mapping for easier testing

--- src/main/java/com/ozoto/diocognizantproj2/controller/personController.java
+++ src/main/java/com/ozoto/diocognizantproj2/controller/personController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/people/v0.0.1/")
+@RequestMapping("/")
 public class personController {
 
     @GetMapping
