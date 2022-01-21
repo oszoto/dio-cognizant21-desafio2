495cd2de339f746be0334963cc07fe0b1b1d7e06
Gabriel M <bielsantos99@gmail.com>
2022-01-21 17:42:18 -0300

    Fix main attribute config

--- src/main/java/com/ozoto/diocognizantproj2/Application.java
+++ src/main/java/com/ozoto/diocognizantproj2/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Diocognizantproj2Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Diocognizantproj2Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
