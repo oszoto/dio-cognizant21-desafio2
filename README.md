c53739338d9868f277fc22c59d94c912577fbc58
Gabriel M <bielsantos99@gmail.com>
2022-01-27 15:17:34 -0300

    Minimally viable project

---
+++ README.md
@@ -0,0 +1,7 @@
+# API Rest para gerenciamento de pessoas
+## Projeto desenvolvido para o bootcamp DIO-Cognizant 2021/2022
+
+Código replicado manualmente a partir do curso apresentado no bootcamp e no código encontrado [neste repositório](https://github.com/rpeleias-v1/personapi_digital_innovation_one).
+Build realizada com Gradle 7.3.3 em ambiente Windows 10 Version 10.0.19043 Build 19043. Informações sobre versões de java e dependências utilizadas no arquivo build.gradle.
+
+[Heroku App](https://dio21-peopleapi-live.herokuapp.com/) (Obs.: Não há GUI)
--- src/main/java/com/ozoto/diocognizantproj2/controller/PersonController.java
+++ src/main/java/com/ozoto/diocognizantproj2/controller/PersonController.java
@@ -21,16 +21,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+
 @RestController
 @RequestMapping("/")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private PersonService personService;
-
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
